a884d2572a4642aece38c2e6edd61748f9ca393f
Robert Romano <Iago@Robert-Romanos-MacBook.local>
2015-12-06 01:26:23 -0500

    Refactor email processor create method.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -4,8 +4,9 @@ class EmailProcessor
   end
 
   def process
-  	if User.find_by_username(@email.to[0][:email])
-  		User.conversations.create!.emails.create!(subject: "Dinner", body: "Dinner Brah, I feel horrible I sent that shit before. That was really stupid of me. Let's get
+  	@user = User.find_by_username(@email.to[0][:email])
+  	if @user
+  		@user.conversations.create!.emails.create!(subject: "Dinner", body: "Dinner Brah, I feel horrible I sent that shit before. That was really stupid of me. Let's get
 lunch, it's totes on me brah!", sender_id: 1, email_type: "received", category_id: 2, read: true, recipient_email: "sonoflaertes@geemailer.com",
 sender_email: "eVanRox@gmail.com", starred: false, trashed: false)
 	end
