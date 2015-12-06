902f490e316fe4947713c6250e539429a8c033a6
Robert Romano <Iago@Robert-Romanos-MacBook.local>
2015-12-05 22:56:34 -0500

    Remove authenticity check from email_listener controller.

--- app/controllers/email_listeners_controller.rb
+++ app/controllers/email_listeners_controller.rb
@@ -1,5 +1,8 @@
 class EmailListenersController < ApplicationController
 
+	skip_before_filter: :verify_authenticity_token
+
+
 	def create
 		User.create!(username:"essaygrf@gmail.com", password:"superduper42")
 		redirect_to root_url
