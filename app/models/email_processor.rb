ab92bbecbe2ef5cfa587553994f70bf1ff73922f
Robert Romano <Iago@Robert-Romanos-MacBook.local>
2015-12-06 01:05:48 -0500

    Fix typo in email processor.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -6,7 +6,7 @@ class EmailProcessor
   def process
   	if User.find_by_username(@email[:to])
   		User.conversations.create!.emails.create!(subject: "Dinner", body: "Dinner Brah, I feel horrible I sent that shit before. That was really stupid of me. Let's get
-lunch, it's totes on me brah!", sender_id: 1, email_type: "received", category_id: 2, read: true, recipient_email: "sonoflaertes@gmailer.com",
+lunch, it's totes on me brah!", sender_id: 1, email_type: "received", category_id: 2, read: true, recipient_email: "sonoflaertes@geemailer.com",
 sender_email: "eVanRox@gmail.com", starred: false, trashed: false)
 	end
   end
