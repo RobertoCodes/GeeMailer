ae48a54a00a506862c58bd7b06d8925363f53088
Robert Romano <Iago@Robert-Romanos-MacBook.local>
2015-12-06 00:59:58 -0500

    Debug e-mail handling.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -7,7 +7,7 @@ class EmailProcessor
   	if User.find_by_username(@email[:to])
   		User.conversations.create!.emails.create!(subject: "Dinner", body: "Dinner Brah, I feel horrible I sent that shit before. That was really stupid of me. Let's get
 lunch, it's totes on me brah!", sender_id: 1, email_type: "received", category_id: 2, read: true, recipient_email: "sonoflaertes@gmailer.com",
-sender_email: "eVanRox@gmail.com", starred: false, trashed: false))
+sender_email: "eVanRox@gmail.com", starred: false, trashed: false)
 	end
   end
 
