f384cf70ace365e96cc19ba02940310f32c033cf
Robert <robertandrewromano@gmail.com>
2015-12-15 06:58:51 -0500

    Debug email processor.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -8,7 +8,7 @@ class EmailProcessor
   	if @user
       parent_email = Email.find_by_message_id(@email.headers[:references])
       if parent_email
-        parent_email.conversation.create!(subject: @email.subject,
+        parent_email.conversation.emails.create!(subject: @email.subject,
         body: @email.body, email_type: "received", read: false,
         recipient_email: @email.to[0][:email], sender_email: @email.from[:email],
         sender_name: @email.from[:name],
