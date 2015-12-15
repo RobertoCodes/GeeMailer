f08c00c6d4e3649ea273e0067a9716439453676a
Robert <robertandrewromano@gmail.com>
2015-12-15 03:28:35 -0500

    Update email processor, removing sender id and category id columns.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -7,7 +7,7 @@ class EmailProcessor
   	@user = User.find_by_username(@email.to[0][:email])
   	if @user
   		@user.conversations.create!.emails.create!(
-  			subject: @email.subject, body: @email.body, sender_id: 1, email_type: "received", category_id: 2, read: false, 
+  			subject: @email.subject, body: @email.body, email_type: "received", read: false,
   			recipient_email: @email.to[0][:email], sender_email: @email.from[:email], sender_name: @email.from[:name],
   			starred: false, trashed: false)
 	 end
