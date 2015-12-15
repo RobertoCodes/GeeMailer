14831b42671e7b71f9516b400e4c9f0fb9358936
Robert <robertandrewromano@gmail.com>
2015-12-15 06:54:52 -0500

    Change email processor to handle message ids.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -6,11 +6,22 @@ class EmailProcessor
   def process
   	@user = User.find_by_username(@email.to[0][:email])
   	if @user
-  		@user.conversations.create!.emails.create!(
-  			subject: @email.subject, body: @email.body, email_type: "received", read: false,
-  			recipient_email: @email.to[0][:email], sender_email: @email.from[:email], sender_name: @email.from[:name],
-  			starred: false, trashed: false)
-	 end
+      parent_email = Email.find_by_message_id(@email.headers[:references])
+      if parent_email
+        parent_email.conversation.create!(subject: @email.subject,
+        body: @email.body, email_type: "received", read: false,
+        recipient_email: @email.to[0][:email], sender_email: @email.from[:email],
+        sender_name: @email.from[:name],
+        starred: false, trashed: false, message_id: @email.headers["message-id"])
+
+      else
+    		@user.conversations.create!.emails.create!(
+    			subject: @email.subject, body: @email.body, email_type: "received",
+          read: false, recipient_email: @email.to[0][:email], sender_email: @email.from[:email],
+          sender_name: @email.from[:name], starred: false, trashed: false, message_id: @email.headers["message-id"])
+      end
+
+	  end
   end
 
 end
