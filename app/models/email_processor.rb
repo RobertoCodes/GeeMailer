ccd822b2d8fff74d656279d3f25397831a369a82
Robert <robertandrewromano@gmail.com>
2015-12-15 18:10:27 -0500

    Debug email processor.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -6,11 +6,12 @@ class EmailProcessor
   def process
   	@user = User.find_by_username(@email.to[0][:email])
   	if @user
+      raw_header = @email.headers
       #Regex expression parses header and returns 'Message-ID' header.
-      message_id = @email.headers[/#{"Message-ID"}(.*?)#{"In-Reply-To"}/m, 1][/#{"<"}(.*?)#{">"}/m, 1]
+      message_id = raw_header[/#{"Message-ID"}(.*?)#{"In-Reply-To"}/m, 1][/#{"<"}(.*?)#{">"}/m, 1]
 
       #Regex expression parses header and returns 'References' header.
-      reference_message_id = @email.headers[/#{"References"}(.*?)#{"Subject"}/m, 1][/#{"<"}(.*?)#{">"}/m, 1]
+      reference_message_id = raw_header[/#{"References"}(.*?)#{"Subject"}/m, 1][/#{"<"}(.*?)#{">"}/m, 1]
 
       parent_email = Email.find_by_message_id(reference_message_id)
       if parent_email
